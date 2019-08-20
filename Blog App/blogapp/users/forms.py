f1c16ce952a46a4ab5595598cbda55bc88db6071
Oktarian <oktotb@gmail.com>
2019-08-20 11:31:53 +0800

    Blog App updates

--- Blog App/blogapp/users/forms.py
+++ Blog App/blogapp/users/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
 from blogapp.models import User
 
 
@@ -29,3 +31,20 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Login")
 
 
+class UpdateAccountForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=2, max=18)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    picture = FileField("Update Profile Picture", validators=[FileAllowed(["jpg", "png"])])
+    submit = SubmitField("Update")
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("This username is taken. Please choose a different one.")
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError("This email is taken. Please choose a different one.")
